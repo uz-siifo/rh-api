ca41c2b6c65e5899de6a7fc0504accc282c9db05
Eloide Novela <eloide.novela@outlook.com>
2024-10-04 11:58:07 +0200

    refactor: codigo de configuracao refactorado, e adicionei alguns comentarios para uma melhor compreensao

--- config/config.py
+++ config/config.py
@@ -1,11 +1,13 @@
 from configparser import ConfigParser
 
+# funcao para carregar as configuracoes do arquivo database.ini
+# param (filename) -> nome do arquivo de configuracao
+# param (section) -> e o grupo de configuracoes que estao no arquivo database.ini
 def load_config(filename='config/database.ini', section='postgresql'):
-    parser = ConfigParser()
+    parser = ConfigParser() # permite ler os chaves e os valores em arquivos de configuracao .ini
     parser.read(filename)
 
-    # get section, default to postgresql
-    config = {}
+    config = {} # configuracao padrao
     if parser.has_section(section):
         params = parser.items(section)
         for param in params:
@@ -13,8 +15,4 @@ def load_config(filename='config/database.ini', section='postgresql'):
     else:
         raise Exception('Sessao: {0}, nao encrontrada em: {1} file'.format(section, filename))
 
-    return config
-
-# if __name__ == '__main__':
-#     config = load_config()
-#     print(config)
+    return config # configuracoes em forma de um dicionario
--- config/connect.py
+++ config/connect.py
@@ -2,28 +2,22 @@ import psycopg2
 from config import load_config
 
 def connect(config):
-    """ Connect to the PostgreSQL database server """
     try:
-        # connecting to the PostgreSQL server
         with psycopg2.connect(**config) as conn:
             print('Conexao estabelecida.')
             return conn
     except (psycopg2.DatabaseError, Exception) as error:
         print(error)
 
-
-# if __name__ == '__main__':
-#     config = load_config()
-#     connect(config)
-
-conn = psycopg2.connect("dbname=hr_system_sinfo user=postgres password=123456789")
+config = load_config()
+conn = connect(config)
 
 cur = conn.cursor()
 
-# cur.execute("insert into department(name, employee_nums, min_salary, max_salary) values(%s, %s, %s, %s)", ("TI", 10, 202070.6, 1777198.66))
-cur.execute("select * from department;")
-res = cur.fetchone()
+# # cur.execute("insert into department(name, employee_nums, min_salary, max_salary) values(%s, %s, %s, %s)", ("TI", 10, 202070.6, 1777198.66))
+# cur.execute("select * from department;")
+# res = cur.fetchone()
 
-print(res)
-conn.commit()
+# print(res)
+# conn.commit()
 # print(res)
