78e99e8f981b4cf237fe57a7b88632d2b8d8439f
Eloide Novela <eloide.novela@outlook.com>
2024-10-05 11:23:11 +0200

    feat: added services

---
+++ service/employee.py
@@ -0,0 +1,13 @@
+
+class employee_service:
+    def create(self, employee):
+        pass
+
+    def update(self, employee):
+        pass
+
+    def delete(self, employee):
+        pass
+
+    def get_all(self):
+        pass
---
+++ service/user.py
@@ -0,0 +1,27 @@
+import sys
+
+# sys.path.append('/sistema-rh-api/config/')
+import config.connect
+
+class user_service:
+    def create(self, user):
+        # conn.e
+        pass
+
+    def update(self, user):
+        pass
+
+    def delete(self, user):
+        pass
+
+    def get_all(self):
+        pass
+
+    def get_by_id(self, id):
+        pass
+
+    def get_by_name(self, name):
+        pass
+
+    def get_by_department(self, department):
+        pass
