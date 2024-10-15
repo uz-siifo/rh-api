fb3849644a0f6fc10435969958a2d02343d2ee38
Eloide Novela <eloide.novela@outlook.com>
2024-10-15 07:26:56 +0200

    test: feitos os testes nos servicos

--- tests.py
+++ tests.py
@@ -1,122 +1,61 @@
-
-# from model.model import *
-
-from model.model import engine
-# from sqlalchemy.orm import Session
-# from model.department import Department
-
-
-# from sqlalchemy import select
+from service.employee import EmployeeService
 from service.user import UserService
 from service.contacts import ContactService
-from service.department import DepartmentService
-from service.employee import EmployeeService
+from service.department  import DepartmentService
+from service.employee_rating import EmployeeRatingService
+from service.goals import GoalsService
+from service.month_records import MonthRecordsService
+from service.performance_evaluation import PerformanceEvaluationService
+from service.presences import PresencesService
 from service.user_employee import UserEmployeeService
-# from model.employee import Employee
-from model.employee_rating import EmployeeRating
-# from utils.enums import AccessLevelEnum
-user_service = UserService(engine)
+from service.working_hours import WorkingHoursService
+from model.models import engine
 
-print(user_service.get_all())
-# user_employee_service = UserEmployeeService(engine)
+employee_service = EmployeeService(engine)
+user_service = UserService(engine)
+contact_service = ContactService(engine)
+department_service = DepartmentService(engine)
+employee_rating_service = EmployeeRatingService(engine)
+goals_service = GoalsService(engine)
+month_records_service = MonthRecordsService(engine)
+performance_evaluation_service = PerformanceEvaluationService(engine)
+presences_service = PresencesService(engine)
+user_employee_service = UserEmployeeService(engine)
+working_hours_service = WorkingHoursService(engine)
+
 # print(user_employee_service.create({
-#     "user_id": 2,
+#     "user_id": 3,
 #     "employee_id": 1
 # }))
 
-# print(user_employee_service.get_all())
-# employee_service = EmployeeService(engine)
-# print(employee_service.get_all())
-
-            # employee = Employee(
-            #     nuit = data.get('nuit'),
-            #     identity_card_bi = data.get('identity_card_bi'),
-            #     salary = data.get('salary'),
-            #     data_admission = data.get('data_admission'),
-            #     department_id = data.get('department_id'),
-            #     academic_level = data.get('academic_level')
-            # )
-
-# from datetime import date
-# print(employee_service.create({
-#     "nuit": 10101010,
-#     "identity_card_bi": "7774484848S988",
-#     "salary": 101092.2,
-#     "data_admission": date.fromisoformat('2019-12-04'),
-#     "department_id": 1
+# print(user_service.create({
+#     "name": "Eloide Simao",
+#     "nickname": "Novela",
+#     "email": "eloide.novela@outlook.com",
+#     "passwd": "1013eloide.novela",
+#     "access_level": "admin",
+#     "contact": "+258876545335"
 # }))
 
-# contact_service = ContactService(engine)
+# from datetime import datetime
+# print(employee_service.create({
+#     'position_at_work': "engineer",
+#     'nuit': 29393919,
+#     'identity_card_bi': "387437754763767A2",
+#     'salary': 199292.091,
+#     'date_admission': datetime.now(),
+#     'academic_level': "higher",
+#     'department_id': 1
+# }))
 
-# dept_service = DepartmentService(engine)
-# # print(dept_service.delete({
-# #     "name": "Medicina"
-# # }))
-
-# print(dept_service.get_all())
-
-# print(dept_service.create({
-#         "name": "Medicina",
-#         "employee_nums": 10,
-#         "min_salary": 1000.8,
-#         "max_salary": 16000.18
-#     })
-# )
-# res = contact_service.get_all()
-# print(res)
-
-# res = contact_service.get_all_by_user({
-#     "user_id": 3
-# })
-
-# print(res)
-# res = contact_service.delete_by_id({
-#     "user_id": 16
-# })
-
-# creating contact
-# res = contact_service.create({
-#     "user_id": 1,
-#     "contact": "+258867640575"
-# })
-
-
-# updating contact
-# res = contact_service.update({
-#     "id": 2,
-#     "contact": "+258876545439"
-# })
-
-# print(res)
-
-# with Session(engine) as session:
-#     result = session.scalar(
-#         select(Department)
-#     )
-
-#     print(result.name)
-
-
-# from model.user import User
-# with Session(engine) as session:
-#     from utils.enums import AccessLevelEnum
-#     result = session.scalar(
-#         select(User).where(User.access_level == AccessLevelEnum.admin)
-#     )
-
-#     print(result.name)
-#     user = User(
-#         name = "Eloide Simao",
-#         nickname = "Novela",
-#         email = "eloide@outlook.com",
-#         passwd = "10134456",
-#         access_level = AccessLevelEnum.admin
-#     )
-
-#     from model.user_contact import UserContact
-#     _contact = UserContact(contact="+258867640575")
-#     user.contacts = [_contact] 
-
-#     session.add(user)
-#     session.commit()
-# select_user()
+print("user_serive: ", user_service.get_all(), "\n\n\n")
+print("employee_service: ", employee_service.get_all(), "\n\n\n")
+print("contact_service: ", contact_service.get_all(), "\n\n\n")
+print("department_service: ", department_service.get_all(), "\n\n\n")
+print("employee_rating_service: ", employee_rating_service.get_all(), "\n\n\n")
+print("goals_service: ", goals_service.get_all(), "\n\n\n")
+print("month_records_service: ", month_records_service.get_all(), "\n\n\n")
+print("performance_evaluation_service: ", performance_evaluation_service.get_all(), "\n\n\n")
+print("presences_service: ", presences_service.get_all(), "\n\n\n")
+print("user_employee_service: ", user_employee_service.get_all_employee(), "\n\n\n")
+print("working_hours_service: ", working_hours_service.get_all(), "\n\n\n")
